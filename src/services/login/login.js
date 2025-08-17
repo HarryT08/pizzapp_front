545b494b31697889a1d089ed310b65ac5ed66b76
EdwinTorrado15 <edwintorrado15@gmail.com>
2025-08-17 16:37:53 -0500

    new setup front bohemia

--- src/services/login/login.js
+++
@@ -1,8 +0,0 @@
-import { bohemiaApi } from "@/api/bohemiaApi";
-
-export const loginUser = async (user) => {
-  const response = await bohemiaApi.post("/auth/login", user);
-  const { token } = response.data
-  localStorage.setItem('jwttoken', token);
-  return response;
-};
